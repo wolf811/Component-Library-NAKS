7def5f91cc86f10d8492d841e5cdac06cccea9ca
wolf811 <wolf811@gmail.com>
2018-11-13 12:53:29 +0300

    добавлен gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
+var plumber = require('gulp-plumber');
+var wait = require('gulp-wait')
+
+gulp.task('browser-sync', ['sass'], function () {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// plumber() - не выбрасывать из компилятора если есть ошибки
+gulp.task('sass', function(){
+    return gulp.src('scss/*.scss')
+        .pipe(wait(500))
+        .pipe(plumber())
+        .pipe(sass({errLogToConsole: true}))
+        // .pipe(sass())
+        .pipe(gulp.dest('css/'))
+        .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
+})
